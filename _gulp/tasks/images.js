b465b34c50e021d1e687676381f8f5e28e924a57
Charlie Rosenbury <gmail@rosenbury.com>
2015-02-21 10:26:14 -0600

    merge in changes from 40d/gulp-starter

--- _gulp/tasks/images.js
+++ _gulp/tasks/images.js
@@ -4,15 +4,10 @@ var changed    = require('gulp-changed'),
     config     = require('../config').images,
     livereload = require('gulp-livereload');
 
-gulp.task('images', function(){
+gulp.task('images', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest))
     .pipe(imagemin())
-    .pipe(gulp.dest(config.dest));
-});
-
-gulp.task('images_dist', function(){
-  return gulp.src(config.src)
-    .pipe(imagemin())
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(config.dest))
+    .pipe(livereload());
 });
--- _gulp/tasks/sass.js
+++ _gulp/tasks/sass.js
@@ -7,22 +7,13 @@ var gulp         = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     livereload   = require('gulp-livereload');
 
-gulp.task('sass', function(){
-    return gulp.src(config.sass.src)
-        .pipe(sourcemaps.init())
-        .pipe(sass(config.sass.settings))
-        .on('error', handleErrors)
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer(config.autoprefixer))
-        .pipe(gulp.dest(config.sass.dest))
-        .pipe(livereload());
-});
-gulp.task('sass_dist', function(){
-    return gulp.src(config.sass.src)
-        .pipe(sourcemaps.init())
-        .pipe(sass(config.sass.settings))
-        .on('error', handleErrors)
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer(config.autoprefixer))
-        .pipe(gulp.dest(config.sass.dest));
+gulp.task('sass', function () {
+  return gulp.src(config.sass.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(config.sass.settings))
+    .on('error', handleErrors)
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(config.autoprefixer))
+    .pipe(gulp.dest(config.sass.dest))
+    .pipe(livereload());
 });
