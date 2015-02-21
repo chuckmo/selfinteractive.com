baac28cae8973e183565471edcf340e4e3779b7f
Charlie Rosenbury <gmail@rosenbury.com>
2015-02-21 11:11:15 -0600

    further styling of splash stuff

--- wp-theme/template-splash.php
+++ wp-theme/template-splash.php
@@ -8,23 +8,26 @@ get_header();
 <header class="splash-header"> 
 	<img class="logo" src="<?php bloginfo('template_directory') ?>/assets/images/logo.png" alt="SELF Interactive" />
 	<h1>Interactive Development,<br/> Consulting &amp; Training</h1>
+	<aside>
+		<ul class="contact-icos">
+			<li class="contact">
+				<a href="mailto:charlie@selfinteractive.com">
+					Contact
+					<img src="<?php bloginfo('template_directory') ?>/assets/images/speech-point.png" class="speech-point" />
+				</a>
+			</li>
+			<li class="twitter">
+				<a href="https://twitter.com/chuckmo">Twitter @chuckmo</a>
+			</li>
+			<li class="github">
+				<a href="https://github.com/chuckmo">Github</a>
+			</li>
+			<li class="linkedin">
+				<a href="https://www.linkedin.com/in/rosenbury">LinkedIn</a>
+			</li>
+		</ul>
+	</aside>
 </header>
-<aside>
-	<ul>
-		<li class="contact">
-			<a href="mailto:charlie@selfinteractive.com">Contact</a>
-		</li>
-		<li class="twitter">
-			<a href="https://twitter.com/chuckmo">Twitter @chuckmo</a>
-		</li>
-		<li class="github">
-			<a href="https://github.com/chuckmo">Github</a>
-		</li>
-		<li class="linkedin">
-			<a href="https://www.linkedin.com/in/rosenbury">LinkedIn</a>
-		</li>
-	</ul>
-</aside>
 <?php
 get_footer();
 ?>
