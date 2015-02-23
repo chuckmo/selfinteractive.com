648d3657494a20f3bf4917396f3c4cc514374a7b
Charlie Rosenbury <gmail@rosenbury.com>
2015-02-23 11:47:59 -0600

    add images task to production task

--- _gulp/tasks/images.js
+++ _gulp/tasks/images.js
@@ -8,6 +8,5 @@ gulp.task('images', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest))
     .pipe(imagemin())
-    .pipe(gulp.dest(config.dest))
-    .pipe(livereload());
+    .pipe(gulp.dest(config.dest));
 });
